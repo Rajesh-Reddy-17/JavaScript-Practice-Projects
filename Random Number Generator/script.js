bf53ed2da8e3979308d5a692c7712802c0908fe9
Rajesh-Reddy-17 <padalarajeshreddy99@gmail.com>
2026-07-06 13:12:59 +0530

    Create script.js

---
+++ Random Number Generator/script.js
@@ -0,0 +1,21 @@
+const myButton = document.querySelector("#myBtn");
+const label1 = document.querySelector("#label1");
+const label2 = document.querySelector("#label2");
+const label3 = document.querySelector("#label3");
+
+const min = 1;
+const max = 6;
+
+let randomNum1;
+let randomNum2;
+let randomNum3;
+
+myButton.onclick = function () {
+    randomNum1 = Math.floor(Math.random() * (max - min + 1) + min);
+    randomNum2 = Math.floor(Math.random() * (max - min + 1) + min);
+    randomNum3 = Math.floor(Math.random() * (max - min + 1) + min);
+
+    label1.textContent = randomNum1;
+    label2.textContent = randomNum2;
+    label3.textContent = randomNum3;
+};
